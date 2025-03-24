7ef91be1740be8a4aae994658c86591f56bc32d2
João Roque Da Silva Neto <joao.neto777@outlook.com>
2025-03-24 10:59:20 -0300

    Add files via upload

---
+++ printfscanf.c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int main()
+{
+  
+  printf("mensagem a ser exibiida");
+  
+  //%d para numeros inteiros 
+  //%f para numeros com ponto flutuante
+  //%s para string
+ 
+  //scanf("%d", &variavel);
+  
+  //%d para numeros inteiros 
+  //%f para numeros com ponto flutuante
+  //%s para string
+    
+    
+  return 0;
+}
