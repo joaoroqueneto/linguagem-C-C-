877ec59f7475c9f31458ad57adc491f48e418edb
João Roque Da Silva Neto <joao.neto777@outlook.com>
2025-03-24 09:29:38 -0300

    Add files via upload

---
+++ contarcaracteres.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char mensagem[] = "ola essa e uma mensagem de exmplo.";
+    
+    int comprimento = strlen(mensagem);
+    
+    printf("a mensagem tem %d caracteres.\n", comprimento);
+    
+    
+  return 0;
+}
