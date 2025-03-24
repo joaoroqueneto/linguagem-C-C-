19744ebdb49bf1b5b9b4c70184ea27f4c0496b7a
João Roque Da Silva Neto <joao.neto777@outlook.com>
2025-03-24 11:06:27 -0300

    Add files via upload

---
+++ digitandoumnumero.c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+int main()
+{
+  
+ int numero;
+ 
+ printf("digite um numero:");
+ 
+ scanf("%d", &numero);
+ 
+ printf("voce digitou %d\n", numero );
+    
+    
+  return 0;
+}
