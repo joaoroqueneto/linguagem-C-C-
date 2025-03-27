a346830a6f6339db7f155617bcdbf7330e7298b2
João Roque Da Silva Neto <joao.neto777@outlook.com>
2025-03-27 08:04:25 -0300

    Add files via upload

---
+++ descobrindocaracteresemumastring.c.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char nome[] = "programaçao";
+    
+    int comprimento = strlen(nome);
+    
+    printf("o comprimento da string e : %d", comprimento);
+    
+    return(0);
+}
